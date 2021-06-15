552692700a8628d7283bb52e978547d9d482f2be
Karl Prieb <karl@prieb.me>
2021-06-15 16:12:40 -0300

    docker image and docker-compose

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+#FROM alpine:3.13
+FROM debian:stable-slim
+
+RUN mkdir -p /writefreely/temp
+#RUN apk add --update curl
+RUN apt-get update && apt-get install curl -y && rm -rf /var/lib/apt/lists/*
+RUN cd /writefreely/temp && \
+  curl -LJO https://github.com/writefreely/writefreely/releases/download/v0.13.0/writefreely_0.13.0_linux_amd64.tar.gz && \
+  tar -xf writefreely_0.13.0_linux_amd64.tar.gz && \
+  mv writefreely/* .. && \
+  cd /writefreely && \
+  rm -rf temp
+
+WORKDIR /writefreely
+
+COPY bin/run.sh .
+
+VOLUME /data
+VOLUME /config
+EXPOSE 8080
+
+ENTRYPOINT ["./run.sh"]
---
+++ bin/run.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -e
+
+CONFIG_FILE=/config/config.ini
+DB_FILE=/data/writefreely.db
+KEY_FILE=/data/keys/email.aes256
+WRITEFREELY="/writefreely/writefreely -c ${CONFIG_FILE}"
+
+if [ ! -s ${CONFIG_FILE} ]; then
+    echo "ERROR: no config.ini file"
+    exit 0
+fi
+
+if [ -e ${DB_FILE} ] && [ -e ${KEY_FILE} ]; then
+    BACKUP="writefreely.$(date +%s).db"
+    cp ${DB_FILE} /data/${BACKUP}
+    ${WRITEFREELY} -migrate
+    if cmp ${DB_FILE} /data/${BACKUP}; then
+        rm /data/${BACKUP}
+    else
+        echo "Database backed up at /data/${BACKUP}"
+    fi
+    exec ${WRITEFREELY}
+fi
+
+if [ ! -s ${DB_FILE} ]; then
+    ${WRITEFREELY} -init-db
+fi
+
+if [ ! -e ${KEY_FILE} ]; then
+    ${WRITEFREELY} -gen-keys
+fi
+
+exec ${WRITEFREELY}
+
